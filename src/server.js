b02c4a8023e31b36bb4e68e5ec34819220bfa6b6
Carlos <carlossouza_94@hotmail.com>
2020-08-16 17:47:32 -0300

    Day 4 complete

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+//Dados
 const proffys = [
     { 
         name: "Diego Fernandes", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "99998888", 
+        whatsapp: "3599998888", 
         bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
         subject: "Química", 
         cost: "20", 
@@ -13,7 +14,7 @@ const proffys = [
     { 
         name: "Diego Fernandes2", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "99998888", 
+        whatsapp: "3599997777", 
         bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
         subject: "Química", 
         cost: "20", 
@@ -23,29 +24,75 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciência",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+//Funcionalidades da aplicação
+
+function getSubject(subjectNumber){
+    const arrayPosition = +subjectNumber - 1
+    return subjects[arrayPosition]
+}
+
 function pageLanding(req, res){
     return res.render("index.html")
 }
 
 function pageStudy(req, res){
-    return res.render("study.html", { proffys })
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res){
-    return res.render("give-classes.html")
+    const data = req.query
+
+    // se tiver dados 
+    const isNotEmpty = Object.keys(data).length > 0
+    if(isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+         //adicionar data a lista de proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+    //se não, mostrar a página
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
+//Servidor
 const express = require('express')
 const server = express()
 
 
-//configurar nunjucks
+//configurar nunjucks(template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
+//inicio e configuração do servidor
 server
 //configurar arquivos estáticos(css, scripts, imagens)
 .use(express.static("public"))
@@ -53,5 +100,5 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-
+//start do servidor
 .listen(5500)
